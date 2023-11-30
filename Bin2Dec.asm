5962af373728aec31dfe41fe6c34d9faa5453e03
NonBlu <sallowsaiden@gmail.com>
2023-11-30 12:58:41 -0800

    Create Bin2Dec.asm

---
+++ Bin2Dec.asm
@@ -0,0 +1,16 @@
+//Your program will accept only six keys from the keyboard:  0, 1, Enter, Backspace, c, and q 
+//(Note: Enter is ASCII decimal 128 and Backspace is ASCII decimal 129). Any other input will be ignored.
+//
+//Your program will accept and display 16 binary digits (although the user can Backspace at any point to
+// make corrections) followed by an Enter. If a Backspace is input, then the previous input digit will be
+// erased (on the display and optionally in memory). If more than 16 bits are entered, then anything
+// after 16 will be ignored (and not displayed). If c is entered at any time, then the entire line 
+//(and optionally in memory) will be cleared. If q is entered at any time, then your program will clear
+// the line and terminate (i.e. go into an infinite loop) immediately.
+//
+//Once the Enter is received after the 16 binary digits, your program will output a right arrow (a minus
+// - followed by greater than >) followed by the decimal equivalent (with a + for positive numbers,
+// including zero,  and a - for negative numbers and NO leading spaces) and will then wait for an Enter
+// (ignoring any other input except c for clear and q for quit). Once an Enter is received, the displayed
+// line will be cleared and your program will wait for new binary digit input
+
