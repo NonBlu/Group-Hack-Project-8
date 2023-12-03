527ba3e563946d5a0ff88b704033d4dabc9da2bd
NonBlu <sallowsaiden@gmail.com>
2023-12-03 13:28:51 -0800

    Update Bin2Dec.asm

--- Bin2Dec.asm
+++ Bin2Dec.asm
@@ -15,4 +15,54 @@
 // line will be cleared and your program will wait for new binary digit input
 
 
-@19
+//One group needs to handle user input, and storing into R0 through R15
+//The other needs to handle receiving from R0 through R15 and displaying result
+//One member does input validation
+//One member does input storage
+//One member does conversion to decimal
+//One member outputs to program
+
+
+
+//first step is grab input validation 
+//recieve input
+//input validation
+//if correct input, display on screen
+//or do command
+
+@as_bitCounter
+D=A
+@0
+M=A //create an incrememnter responsible for tracking how many bits are entered
+
+@ge_currentColumn //tracks cursor
+M=0
+
+@KBD
+M=0 //when there is no input, kbd is set to a value less than 1
+(as_getKey) //infinite loop that runs until a valid input is pressed
+
+    @KBD    // get value of keyboard
+    D=M
+
+    @as_getKey
+    D;JLE //if user enters a value, kbd will no longer be -1 
+
+    //user has entered a key, must now validaate input
+
+    //if statements to validate input
+    //first check if as_bitCounter is <=16 
+    @as_userInput
+    M=D
+    @as_bitCounter
+    D=M
+    @16
+    D=A-D //if bit counter is 16, will be 0. if bit counter is less than 16, will be positive
+    @as_checkOneZero
+    D;JGE //executes if bits <=16. jumps if greater than or equal to 0
+
+    (as_checkOneZero) //checks if input is 0 or 1 
+        @as_userInput
+        D=M
+
+        //if zero, then jump to kp_outputKey 
